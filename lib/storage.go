1a0eac401999d4cff2e36157979e53ee39af0301
Chris Miceli <git@miceli.net.au>
2019-05-27 16:54:00 -0700

    Adding FS Interface

--- lib/storage.go
+++ lib/storage.go
@@ -40,3 +40,51 @@ func (m *MemStorage) LastUpdate(identifier string) (time.Time, error) {
 	}
 	return time.Now(), NoSuchFile
 }
+
+type FSStorage struct {
+	dir string
+}
+
+func (m *FSStorage) Get(identifier string) (Config, error) {
+	log.Printf("FSStorage: Get(%s)", identifier)
+	var x Config
+	f, err := os.Open(m.dir + identifier)
+	if err != nil {
+		log.Printf("FSStorage: failed to open %s with %v", identifier, err)
+		return err
+	}
+	defer f.Close()
+	dec := json.NewDecoder(f)
+	err = dec.Decode(&x)
+	return x, err
+}
+
+func (m *FSStorage) Set(identifier string, cfg Config) error {
+	log.Printf("FSStorage: Set(%s)", identifier)
+	cfg.Date = time.Now()
+	f, err := os.OpenFile(m.dir+identifier, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		log.Printf("FSStorage: failed to open %s with %v", identifier, err)
+		return err
+	}
+	defer f.Close()
+	b, err := json.Marshal(cfg)
+	if err != nil {
+		log.Printf("FSStorage: failed to serialise %s", identifier)
+		return err
+	}
+	_, err = f.Write(b)
+	return err
+}
+
+func (m *FSStorage) LastUpdate(identifier string) (time.Time, error) {
+	f, err := os.Lstat(m.dir + identifier)
+	if err != nil {
+		return nil, err
+	}
+	return f.ModTime(), nil
+}
+
+func NewFSStorage(dir string) Storage {
+	return &FSStorage{dir: dir}
+}
